e37733ca2ab6b07278f65f6699a0454ac0a3398e
uxhawk <uxhawk@gmail.com>
2026-04-24 17:24:58 -0700

    updated top nav margin

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -17,7 +17,7 @@ export function ToastProvider({ children }) {
     <ToastContext.Provider value={showToast}>
       {children}
       {toasts.length > 0 && (
-        <div className="fixed top-4 inset-x-4 z-50 flex flex-col gap-2 pointer-events-none">
+        <div className="fixed z-50 flex flex-col gap-2 pointer-events-none inset-x-4 top-[calc(1rem+env(safe-area-inset-top,0px))]">
           {toasts.map(toast => (
             <div
               key={toast.id}
